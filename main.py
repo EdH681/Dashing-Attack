248307da8d07d0bfb4254d9c4aa0f93ac0d82bbe
EdH681 <edwardhornby07@gmail.com>
2024-02-29 00:22:46 +0000

    Got the dash feature working with a strange bug

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class Arena:
         self.block = 75
         self.grid = get_grid()
         self.__display = self.__create()
+        img = pygame.image.load("assets/arena/background.png").convert()
+        self.background = pygame.transform.scale(img, (1500, 750))
+        self.colour = "green"
 
     def __create(self):
         pygame.init()
@@ -27,29 +30,38 @@ class Arena:
         return self.__display
 
     def run(self):
+        self.__display.blit(self.background, (0, 0))
         self.__draw()
 
 
 class Player:
-    def __init__(self, display):
+    def __init__(self, display: pygame.display):
         self.display = display
+        self.colour = "green"
         self.x = 750
         self.y = 375
         self.y_vel = 0
         self.x_vel = 0
+        self.ready = True
+        self.charge = 100
+        self.cost = 20
+        self.recharge = 1
+        self.x_dash = 0
+        self.y_dash = 0
+        self.DASH = 50
         self.FRICTION = 1.1
 
     def __move(self):
         vel = 10
         key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            self.y_vel = vel
-        if key[pygame.K_DOWN]:
-            self.y_vel = -vel
-        if key[pygame.K_RIGHT]:
-            self.x_vel = vel
-        if key[pygame.K_LEFT]:
-            self.x_vel = -vel
+        if key[pygame.K_UP] or key[pygame.K_w]:
+            self.y_vel = vel + self.y_dash
+        if key[pygame.K_DOWN] or key[pygame.K_s]:
+            self.y_vel = -vel + self.y_dash
+        if key[pygame.K_RIGHT] or key[pygame.K_d]:
+            self.x_vel = vel + self.x_dash
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            self.x_vel = -vel + self.x_dash
         if self.x_vel or self.y_vel:
             unit = math.sqrt(self.x_vel**2 + self.y_vel**2)
             if self.y_vel:
@@ -59,11 +71,41 @@ class Player:
             self.y_vel /= self.FRICTION
             self.x_vel /= self.FRICTION
 
+    def __dash(self):
+        magnitude = math.sqrt(self.x_vel**2 + self.y_vel**2)
+        try:
+            x_unit = self.x_vel/magnitude
+            y_unit = self.y_vel/magnitude
+        except ZeroDivisionError:
+            x_unit, y_unit = 0, 0
+
+        print((pygame.key.get_pressed()[pygame.K_SPACE]) or "")
+
+        if math.fabs(self.x_dash) < 0.1:
+            self.x_dash = 0
+        if math.fabs(self.y_dash) < 0.1:
+            self.y_dash = 0
+
+        if pygame.key.get_pressed()[pygame.K_SPACE] and self.ready:
+            self.colour = "red"
+            self.x_dash = x_unit * self.DASH
+            self.y_dash = y_unit * self.DASH
+            self.charge -= self.cost
+            self.ready = False
+        if not pygame.key.get_pressed()[pygame.K_SPACE]:
+            self.ready = True
+            self.colour = "green"
+        if self.charge < 100:
+            self.charge += self.recharge
+        self.x_dash /= self.FRICTION
+        self.y_dash /= self.FRICTION
     def __draw(self):
-        pygame.draw.circle(self.display, "green", (self.x, self.y), 20)
+        pygame.draw.circle(self.display, self.colour, (self.x, self.y), 20)
 
     def run(self):
+
         self.__move()
+        self.__dash()
         self.__draw()
 
 
@@ -93,7 +135,6 @@ def run():
     running = True
     while running:
         clock.tick(60)
-        win.fill("black")
         ##
         arena.run()
         player.run()
